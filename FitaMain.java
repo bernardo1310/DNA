4da6b5e9c5b69a495906da6e92eaa737a90c4ebb
Bernardo França Coelho <112012405+bernardo1310@users.noreply.github.com>
2024-10-10 10:34:57 -0300

    Add files via upload

---
+++ FitaMain.java
@@ -0,0 +1,70 @@
+
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner; 
+
+public class FitaMain {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        int totalFitas = 0, totalFitasValidas = 0, totalFitasInvalidas = 0;
+        List<Integer> linhasInvalidas = new ArrayList<>();
+
+        String caminhoArquivoOriginal = "C:\\Users\\alunos\\Desktop\\bernardoPOO\\ProjetPOO\\src\\FitaDNA.txt";
+        String caminhoArquivoSaida = "C:\\\\Users\\\\alunos\\\\Desktop\\\\bernardoPOO\\\\ProjetPOO\\\\src\\\\DNAFINAL.txt";
+
+        try {
+            File arquivoEntrada = new File(caminhoArquivoOriginal);
+            PrintWriter arquivoSaida = new PrintWriter(caminhoArquivoSaida, "UTF-8");
+            Scanner leitor = new Scanner(arquivoEntrada);
+            
+            while (leitor.hasNextLine()) {
+                String fita = leitor.nextLine();
+                totalFitas++;
+
+                boolean fitaValida = true; 
+                String fitaComplementar = "";
+
+                for (char dup : fita.toCharArray()) {
+                    if (dup != 'G' && dup != 'A' && dup != 'T' && dup != 'C') {
+                        fitaValida = false; 
+                        break;
+                    } 
+                }
+
+                if (fitaValida) {
+                    for (char dup : fita.toCharArray()) {
+                        if (dup == 'G') 
+                            fitaComplementar += 'C';
+                        else if (dup == 'C')
+                            fitaComplementar += 'G';
+                        else if (dup == 'T')
+                            fitaComplementar += 'A';
+                        else 
+                            fitaComplementar += 'T';
+                    }
+                    arquivoSaida.println(fitaComplementar);
+                    totalFitasValidas++;
+                } else {
+                	arquivoSaida.println("***FITA INVÁLIDA - " + fita);
+                    totalFitasInvalidas++;
+                    linhasInvalidas.add(totalFitas);
+                }
+            }
+
+            leitor.close();
+            arquivoSaida.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Um erro ocorreu.");
+            e.printStackTrace();
+        }
+
+        System.out.println("O total de fitas é: " + totalFitas);
+        System.out.println("O total de fitas válidas é: " + totalFitasValidas);
+        System.out.println("O total de fitas inválidas é: " + totalFitasInvalidas);
+        System.out.println("As linhas inválidas são: " + linhasInvalidas);
+    }
+}
